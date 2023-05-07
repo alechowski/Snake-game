aee7bf953c19016103898b31e98ed4c94ca459c7
alechowski <lechowski.andrzej@outlook.com>
2023-05-07 17:38:09 +0200

    feat: set basic functions

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 640;
+canvas.height = 640;
 
 const cw = canvas.width;
 const ch = canvas.height;
 
 const size = 20;
 
-ctx.fillStyle = 'lime';
-ctx.fillRect((cw/2) - size/2,(ch/2) - size/2, size ,size);
+function game () {
 
+    gameBoard();
+    drawSnake();
+    drawFood();
+}
+
+function gameBoard () {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,cw,ch);
+}
+
+function drawSnake () {
+    ctx.fillStyle = 'lime';
+    ctx.fillRect((cw/2) - size/2,(ch/2) - size/2, size ,size);
+}
+
+function drawFood () {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(Math.round(Math.random() * 32) * 20, Math.round(Math.random() * 32) * 20, size, size);
+}
+
+game()

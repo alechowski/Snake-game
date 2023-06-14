4cb16c47bffd3b928b477c1566d11567a52e0dea
alechowski <lechowski.andrzej@outlook.com>
2023-06-14 22:29:31 +0200

    refactor: set the initial values and correct the typo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext("2d");
-const displayScore = document.querySelector('.lenght')
+const displayScore = document.querySelector('.length')
 
 canvas.width = 640;
 canvas.height = 640;
